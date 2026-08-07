5f5c9a35d09d265df31c6bd0fb63fab76b9ad947
Hector0420 <106408698+Hector0420@users.noreply.github.com>
2026-08-07 12:48:25 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// js/config.js
+const SUPABASE_URL = 'https://ojglxzidivvflbscwptd.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'https://ojglxzidivvflbscwptd.supabase.co/auth/v1/callback';
+const GA_MEASUREMENT_ID = 'G-XXXXXXXX'; // Opcional, déjalo vacío si no usas// JavaScript Document
