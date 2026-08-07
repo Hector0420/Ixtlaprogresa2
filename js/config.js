cac6324944770f1a0c9f491e0f4561b6e01c2f2a
Hector0420 <106408698+Hector0420@users.noreply.github.com>
2026-08-07 16:13:19 -0600

    Update config.js
    cambio de anon key o llave publica

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // js/config.js
 const SUPABASE_URL = 'https://ojglxzidivvflbscwptd.supabase.co/rest/v1/';
-const SUPABASE_ANON_KEY = 'https://ojglxzidivvflbscwptd.supabase.co/auth/v1/callback';
+const SUPABASE_ANON_KEY = 'sb_publishable_2WbBQi0Odt5DGxR0pQVJEQ_erCCaPPF';
 const GA_MEASUREMENT_ID = 'G-XXXXXXXX'; // Opcional, déjalo vacío si no usas// JavaScript Document
